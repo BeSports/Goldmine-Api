051bd767ccbbf49742b93a17ebe303e9f7065c5c
Michiel Cuvelier <cuvelier.michiel@gmail.com>
2017-11-28 14:22:24 +0100

    Removed the logs

--- src/operations/execute.js
+++ src/operations/execute.js
@@ -3,7 +3,6 @@ const { selectBuilder } = require('../builders/OrientDbQueryBuilder');
 const resolver = require('../resolvers/OrientDbQueryResolver');
 
 const execute = async (db, query, params) => {
-  console.log(this);
   const res = await resolver(db, query, params, {}, false);
   return res;
 };
